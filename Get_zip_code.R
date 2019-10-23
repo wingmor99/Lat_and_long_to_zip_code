845739d95a8bd186874dec81b64711698cbd0308
wingmor99 <46871885+wingmor99@users.noreply.github.com>
2019-10-23 16:14:42 -0400

    get the code csv

--- Get_zip_code.R
+++ Get_zip_code.R
@@ -17,8 +17,23 @@ which.min(getDistanse(code$X2, code$X3, crime$Lat[1], crime$Long[1]))
 code$X1[57]
 
 getthecode <- function(Lat2, Lon2) {
+  if (is.na(Lat2) | is.na(Lon2)) {
+    return(NA)
+  }
   ind <- which.min(getDistanse(code$X2, code$X3, Lat2, Lon2))
   return(code$X1[ind])
 }
 
 getthecode(crime$Lat[1], crime$Long[1])
+
+final <- crime %>% mutate(zip=NA)
+
+start.time <- Sys.time()
+for (i in 1:426820){
+  final$zip[i] <- (getthecode(crime$Lat[i], crime$Long[i]))
+}
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+
+write.csv(final,"crime_with_zip.csv")
